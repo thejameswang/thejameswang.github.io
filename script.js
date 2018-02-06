bd80fce3b3a7ce0de4faff8d12e27f81a307adff
James Wang <j.wang5441@gmail.com>
2018-02-05 20:26:24 -0800

    Organizes files

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+  });
+})
