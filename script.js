5f9eaf0e0d1ede967411a02c3a8296947096a1e9
James Wang <j.wang5441@gmail.com>
2018-04-24 14:10:30 -0700

    adds revealing filter for html divs

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 window.sr = ScrollReveal({reset: true});
 $(document).ready(function() {
-  $('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
-  });
+    $(".filter-button1").click(function(){
+        var value = $(this).attr('data-filter1');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter1').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter1").not('.'+value).hide('3000');
+            $('.filter1').filter('.'+value).show('3000');
+
+        }
+    });
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+
+        }
+    });
+    $('a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+    sr.reveal('.dumb')
+    sr.reveal('.about-me')
+    sr.reveal('.experience-container')
+    //Project reveal
+    sr.reveal('.Projects')
+    sr.reveal('.reveal', {duration:500, reset:true}, 50)
+    //Contact
+    sr.reveal('.contact-container', {duration: 1000}, 50)
 })
-sr.reveal('.dumb')
-sr.reveal('.about-me')
-sr.reveal('.experience-container')
-//Project reveal
-sr.reveal('.Projects')
-sr.reveal('.reveal', {duration:500, reset:true}, 50)
-//Contact
-sr.reveal('.contact-container', {duration: 1000}, 50)
