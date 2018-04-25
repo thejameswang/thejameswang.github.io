c5094665c79e66bbf83cbd4b5ebd83e8e5cf6a62
James Wang <j.wang5441@gmail.com>
2018-04-25 10:53:02 -0700

    Cleans up code

--- script.js
+++ script.js
@@ -1,51 +1,36 @@
-window.sr = ScrollReveal({reset: true});
 $(document).ready(function() {
+    window.sr = ScrollReveal({reset: true});
+    // Filters project container
     $(".filter-button1").click(function(){
         var value = $(this).attr('data-filter1');
-
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
+        if(value == "all") {
             $('.filter1').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        } else {
             $(".filter1").not('.'+value).hide('3000');
             $('.filter1').filter('.'+value).show('3000');
-
         }
     });
+    // Filters Hobby container
     $(".filter-button").click(function(){
         var value = $(this).attr('data-filter');
-
-        if(value == "all")
-        {
-            //$('.filter').removeClass('hidden');
+        if(value == "all") {
             $('.filter').show('1000');
-        }
-        else
-        {
-//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
-//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        } else {
             $(".filter").not('.'+value).hide('3000');
             $('.filter').filter('.'+value).show('3000');
-
         }
     });
-    $('a').click(function(){
+    // Allows smooth scrolling for each anchor tag
+    $('a').click(function() {
         $('html, body').animate({
             scrollTop: $( $(this).attr('href') ).offset().top
         }, 500);
         return false;
     });
+    // Does scroll revealing
     sr.reveal('.dumb')
     sr.reveal('.about-me')
     sr.reveal('.experience-container')
-    //Project reveal
-    // sr.reveal('.Projects')
     sr.reveal('.reveal', {duration:500, reset:true}, 30)
-    //Contact
     sr.reveal('.contact-container', {duration: 1000}, 50)
 })
