ac7c966b9ca65aceea65df436c5e2616ae129a80
thejameswang <jwang864@gatech.edu>
2019-05-13 08:16:41 -0700

    fix: button

--- script.js
+++ script.js
@@ -48,9 +48,35 @@ $(document).ready(function() {
   sr.reveal(".reveal", { duration: 1000, reset: true }, 30);
   sr.reveal(".contact-container", { duration: 1000 }, 50);
 
-  var coll = document.getElementsByClassName("collapsible");
+  //   var coll = document.getElementsByClassName("collapsible");
 
-  $("#cE").click(function() {});
+  $(".header").click(function() {
+    $header = $(this);
+    //getting the next element
+    $content = $header.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $content.slideToggle(500, function() {
+      //execute this after slideToggle is done
+      //change text of header based on visibility of content div
+      $header.text(function() {
+        //change text based on condition
+        return $content.is(":visible") ? "Collapse" : "Expand";
+      });
+    });
+  });
+  //   for (i = 0; i < coll.length; i++) {
+  //     coll[i].addEventListener("click", function() {
+  //       this.classList.toggle("active");
+  //       var content = this.nextElementSibling;
+  //       if (content.style.display === "block") {
+  //         content.style.display = "none";
+  //       } else {
+  //         content.style.display = "block";
+  //       }
+  //     });
+  //   }
+
+  //   $("#cE").click(function() {});
 });
 
 // function
